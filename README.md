2b431880a95b91b4d0a90e6faffbe9848144e7a7
tim <tmsteem82@gmail.com>
2021-09-26 22:57:54 +0200

    implement encoding of jwt

---
+++ README.md
@@ -0,0 +1,17 @@
+# Rust JWT tutorial
+
+This code repository shows a basic example of how to use the jsonwebtoken Rust crate.
+
+It shows encoding, decoding and validation of a token with simple claims.
+
+There is an article on my blog explaining the details of this example: [How to use JWT with Rust: learn the basics here](https://tms-dev-blog.com/how-to-use-jwt-with-rust-learn-the-basics/).
+
+## Install & Run
+
+Simply run the project using cargo:
+
+```bash
+cargo run
+```
+
+The output should be token information.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use chrono::{Duration, Utc};
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Claims {
+    sub: String,
+    iat: usize,
+    exp: usize,
+    test: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let key = b"secret";
+    let my_iat = Utc::now().timestamp();
+    let my_exp = Utc::now()
+        .checked_add_signed(Duration::seconds(5))
+        .expect("invalid timestamp")
+        .timestamp();
+    let my_claims = Claims {
+        sub: "h@d.com".to_owned(),
+        iat: my_iat as usize,
+        exp: my_exp as usize,
+        test: "hello world".to_owned(),
+    };
+
+    let token = match encode(
+        &Header::default(),
+        &my_claims,
+        &EncodingKey::from_secret(key),
+    ) {
+        Ok(t) => t,
+        Err(_) => panic!(),
+    };
+
+    println!("token: {:?}", token);
 }
